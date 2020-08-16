d6a2389c826d7afe6f0448a7a22505867148178c
Pedro Goncalves <pedrogoncalves@Pedros-MBP.Home>
2020-08-16 14:12:02 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>Something happened</h1>
+        <h1>Featured Branch</h1>
       </header>
     </div>
   );
